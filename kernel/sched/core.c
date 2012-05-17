fe2634e36a73c8213e6de259a398f46859cd91e3
Heechul Yun <heechul@illinois.edu>
2012-05-16 23:39:27 -0500

    sched: throttle/unthrottle core
    Added two minimum kernel APIs that are needed to support
    memory bandwidth regulation.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7805,6 +7805,141 @@ const u64 min_cfs_quota_period = 1 * NSEC_PER_MSEC; /* 1ms */
 
 static int __cfs_schedulable(struct task_group *tg, u64 period, u64 runtime);
 
+#ifdef CONFIG_SCHED_EVENT_THROTTLE
+
+#include <linux/slab.h>
+static DEFINE_PER_CPU(struct list_head, tq_list);
+struct tq_item {
+        struct task_struct *p;
+        struct list_head task_list;
+};
+static struct kmem_cache *tq_cachep = NULL;
+
+void tq_cache_init(void)
+{
+        tq_cachep = kmem_cache_create(
+                "throttle_cache",
+                sizeof(struct tq_item),
+                0, /* alignment */
+                SLAB_PANIC, /* flags */
+                NULL);
+
+        printk(KERN_INFO "cache %s is created\n", 
+               (tq_cachep->name) ? tq_cachep->name: "NULL");
+}
+
+/**
+ * throttle tasks on a specific cpu
+ *
+ * @cpu target cpu
+ * @return the number of throttled tasks.
+ *
+ * [contract]
+ * - must be reenterrant. can be called in the interrupt context
+ */
+int throttle_rq_cpu(int cpu)
+{
+        struct rq *rq ;
+        struct cfs_rq *cfs_rq;
+        unsigned long flags;
+        struct task_struct *g, *p;
+        struct list_head *head = &per_cpu(tq_list, cpu);
+        int throttle_cnt = 0;
+
+        rq = cpu_rq(cpu);
+        cfs_rq = &rq->cfs; /* no cgroup support */
+
+
+        if (!raw_spin_trylock_irqsave(&rq->lock, flags)) {
+                trace_printk("failed to get rq->lock\n");
+                return -1;
+        }
+
+        INIT_LIST_HEAD(head);
+        list_for_each_entry_safe(p, g, &rq->cfs_tasks, se.group_node) {
+                struct tq_item *item;
+
+                /* do not touch time sensitive kernel threads e.g., softirqd */
+                if (p->flags & PF_WQ_WORKER) /* let's not touch workers */
+                        continue;
+
+                /* add to throttled list */
+                item = (struct tq_item *)kmem_cache_alloc(tq_cachep,
+                                                          GFP_ATOMIC);
+
+                if (unlikely(item == NULL)) {
+                        trace_printk("ERR: alloc failed.\n");
+                        goto out;
+                }
+                /*
+                 * dequeue the task
+                 *   cfs_rq->h_nr_running-- && rq->nr_running-- is done here
+                 *   ->dequeue_task_fair()
+                 */
+                if (p->on_rq) {
+                        item->p = p;
+                        INIT_LIST_HEAD(&item->task_list);
+                        list_add(&item->task_list, head);
+                        throttle_cnt++;
+
+                        deactivate_task(rq, p, DEQUEUE_SLEEP);
+                        p->on_rq = 0;
+                        trace_printk(" p%d(%s,%d)\n", throttle_cnt, p->comm, p->pid);
+                } else {
+                        trace_printk(" p%d(%s,%d) is not in rq. why???\n",
+                                     throttle_cnt, p->comm, p->pid);
+                }
+        }
+
+        resched_task(cpu_curr(cpu));
+out:
+        /* TODO: handle realtime class */
+        raw_spin_unlock_irqrestore(&rq->lock, flags);
+        return throttle_cnt;
+}
+
+int unthrottle_rq_cpu(int cpu)
+{
+        struct rq *rq ;
+        unsigned long flags;
+        struct tq_item *item, *q;
+        struct list_head *head = &per_cpu(tq_list, cpu);
+        int unthrottle_cnt = 0;
+
+        rq = cpu_rq(cpu);
+        if (!raw_spin_trylock_irqsave(&rq->lock, flags)) {
+                trace_printk("failed to get rq->lock\n");
+                return -1;
+        }
+
+        list_for_each_entry_safe(item, q, head, task_list) {
+                BUG_ON(item == NULL || item->p == NULL);
+                /* WARN_ON(item->p->on_rq); */
+                /* WARN_ON(task_cpu(item->p) != cpu ); */
+                /* HEECHUL */
+                if (!item->p->on_rq) {
+                        activate_task(rq, item->p, ENQUEUE_WAKEUP);
+                        item->p->on_rq = 1;
+                } else {
+                        trace_printk("ERR: (%s,%d) is on %d\n",
+                                item->p->comm, item->p->pid, task_cpu(item->p));
+                }
+                list_del_init(&item->task_list);
+                unthrottle_cnt++;
+                trace_printk(" p%d(%s,%d)\n", unthrottle_cnt, item->p->comm, item->p->pid);
+                kmem_cache_free(tq_cachep, item);
+        }
+
+        resched_task(cpu_curr(cpu));
+        raw_spin_unlock_irqrestore(&rq->lock, flags);
+        return unthrottle_cnt;
+}
+
+EXPORT_SYMBOL(throttle_rq_cpu);
+EXPORT_SYMBOL(unthrottle_rq_cpu);
+
+#endif /* CONFIG_SCHED_EVENT_THROTTLE */
+
 static int tg_set_cfs_bandwidth(struct task_group *tg, u64 period, u64 quota)
 {
 	int i, ret = 0, runtime_enabled, runtime_was_enabled;
